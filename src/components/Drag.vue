28f2234132060257b8c010cf559d3adbaa091af5
Gurdeep Kaur <kaur12@outlook.com>
2020-06-04 13:54:38 +0200

    first commit

--- src/components/Drag.vue
+++ src/components/Drag.vue
@@ -3,14 +3,14 @@
     <!-- Footer Links -->
     <div class="container-fluid text-center text-md-left bg-secondary">
       <!-- Grid row -->
-      <div class="row">
+      <div class="row h-100">
         <!-- Grid column -->
         <div class="col-md-2 mt-md-0 mt-3">
           <!-- Content -->
           <h5 class="text-uppercase">Footer Content</h5>
-          <p>Here add contact details</p>
+          <p>some text</p>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 h-100">
           <draggable
             :list="list"
             :disabled="!enabled"
@@ -21,16 +21,35 @@
             @end="dragging = false"
           >
             <transition-group class="listitem">
-              <div class="list" v-for="(element, index) in list" :key="index">
-                <h5 @click="editMenu(element, index)">
-                  <div class="form-group">
-                    <div class="col-4">
-                      <h5 data-toggle="modal" data-target="#add">
-                        <i class="fa fa-pencil-square-o" aria-hidden="true">{{ element.menu }}</i>
-                      </h5>
-                    </div>
-                  </div>
-                </h5>
+              <div class="list card" v-for="(element, index) in list" :key="index">
+                <h3
+                  data-toggle="modal"
+                  data-target="#add"
+                  @click="editMenu(element, index)"
+                  class="card-header"
+                >
+                  <i class="fa fa-pencil-square-o" aria-hidden="true">{{ element.menu }}</i>
+                </h3>
+              </div>
+            </transition-group>
+          </draggable>
+
+          <draggable
+            :listitems="listitems"
+            :disabled="!enabled"
+            class="list-group d-flex"
+            ghost-class="ghost"
+            :move="checkMove"
+            @start="dragging = true"
+            @end="dragging = false"
+          >
+            <transition-group class="listitems mx-200">
+              <div v-for="(item, index) in listitems" :key="index">
+                <ul data-toggle="modal" data-target="#add">
+                  <li>
+                    <i class="fa fa-pencil-square-o" aria-hidden="true">{{ item.link }}</i>
+                  </li>
+                </ul>
               </div>
             </transition-group>
           </draggable>
@@ -138,6 +157,11 @@ export default {
         { menu: "Info", url: "/test", checked: false },
         { menu: "Account", url: "/", checked: false },
         { menu: "Social", url: "/", checked: false }
+      ],
+      listitems: [
+        { link: "facebook", url: "" },
+        { link: "twitter", url: "" },
+        { link: "instagram", url: "" }
       ],
       dragging: false,
       checked: false,
